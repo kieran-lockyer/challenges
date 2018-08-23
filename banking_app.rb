9514782d531435c497b9156b3700ab274d1a0508
kieran-lockyer <kieran.lockyer.87@gmail.com>
2018-08-24 09:02:23 +1000

    adding functionality for multiple users with unique balance and transaction history, incomplete

--- banking_app.rb
+++ banking_app.rb
@@ -1,65 +1,80 @@
+print %x{clear}
+
 begin
     set_balance = File.open("balance.txt", 'a+') { |file| file.readline}.to_i
 rescue EOFError => e
     set_balance = 0
 end
 
-balance = set_balance
-password = "pass"
-
 users = Hash.new()
 
-transaction_history = []
-
-def authenticate(password)
-    print "Please enter your password: "
-    if gets.chomp == password
-        return true
-    else
-        puts "Invalid password!"
-        return false
+def authenticate(users)
+    attempts = 3
+    while attempts > 0
+        puts "#{attempts} attempts remaining"
+        print "Please enter your username: "
+        username = gets.chomp
+        print "Please enter your password: "
+        password = gets.chomp
+        if users[username] == password
+            puts "Welcome #{username}"
+            return username, users[username]["balance"], users[username]["history"]
+        else
+            puts "Your username or password are incorrect"
+            attempts -= 1
+            puts
+        end
     end
+    puts "You have used all available attempts. Please try again later"
+    return false, false, false
 end
 
-def login()
-end
-
-def signup()
+def signup(users)
+    print "Please enter a username: "
+    username = gets.chomp
+    print "Please enter a password: "
+    password = gets.chomp
+    users[username] = {"password": password,
+                       "balance": 0,
+                       "history": []}
+    return users
 end
 
 def display_balance(balance)
     puts "Your balance is $#{balance}"
 end
 
-def deposit(balance, transaction_history)
+def deposit(balance, history)
     print "How much would you like to depost: "
     deposit_amount = gets.chomp.to_i
     balance += deposit_amount
-    transaction_history.push("Deposited $#{deposit_amount}")
+    history.push("Deposited $#{deposit_amount}")
     puts "Your new balance is #{balance}"
-    return balance, transaction_history
+    return balance, history
 end
 
-def withdraw(balance, transaction_history)
+def withdraw(balance, history)
     print "How much would you like to withdraw: "
     withdraw_amount = gets.chomp.to_i
     if balance >= withdraw_amount
         balance -= withdraw_amount
-        transaction_history.push("Withdrew $#{withdraw_amount}")
+        history.push("Withdrew $#{withdraw_amount}")
         puts "Your new balance is #{balance}"
-        return balance, transaction_history
+        return balance, history
     else
         puts "Insufficient funds!"
     end
 end
 
-def history(transaction_history)
-    transaction_history.each { |transaction|
+def history(history)
+    history.each { |transaction|
         puts transaction
     }
 end
 
-def main(balance, transaction_history)
+def exit(users, username, balance, history)
+
+def main(users, balance, history)
     running = true
     while running
         puts "What would you like to do? (Options: balance, deposit, withdraw, history, exit)"
@@ -68,13 +83,13 @@ def main(balance, transaction_history)
         when "balance"
             display_balance(balance)
         when "deposit"
-            balance, transaction_history = deposit(balance, transaction_history)
+            balance, history = deposit(balance, history)
         when "withdraw"
-            balance, transaction_history = withdraw(balance, transaction_history)
+            balance, history = withdraw(balance, history)
         when "history"
-            history(transaction_history)
+            history(history)
         when "exit"
-            File.open("balance.txt", 'w') { |file| file.write(balance)}
+            File.open("balance.txt", 'w') { |file| file.write(users)}
             running = false
             break
         else
@@ -82,14 +97,14 @@ def main(balance, transaction_history)
         end
         print "Would you like to return to the main menu? (Y/N): "
         if gets.chomp == "N"
-            File.open("balance.txt", 'w') { |file| file.write(balance)}
+            File.open("balance.txt", 'w') { |file| file.write(users)}
             running = false
             break
         else
             print %x{clear}
         end
     end
-    return balance, transaction_history
+    return balance, history
 end
 
 running = true
@@ -100,8 +115,9 @@ while running
     choice = gets.chomp
     case choice
     when "l"
-        if authenticate(password)
-            balance, transaction_history = main(balance, transaction_history)
+        username, balance, history = authenticate(users)
+        if balance
+            balance, history = main(balance, history)
         end
     when "e"
         running = false
