b04e049fb04c7a7b3276dd834dd0b1c3b6a20190
kieran-lockyer <kieran.lockyer.87@gmail.com>
2018-08-29 08:22:05 +1000

    first step of class challenge, pre classroom lessons

--- class-challenge.rb
+++ class-challenge.rb
@@ -1,13 +1,25 @@
 class Cat
-    def initialize(name)
+    def initialize(name, walks = 0)
         @name = name
+        @walks = walks
     end
 
     def speak
         puts "#{@name} says meow."
+        return self
+    end
+
+    def walks
+        @walks += 1
+        return self
+    end
+
+    def display_walks
+        puts "#{@name} has been for #{@walks} walks today."
+        return self
     end
 end
 
-cat = Cat.new("Chairman meow")
+cat = Cat.new("Chairman Meow")
 
-cat.speak
+cat.speak.walks.walks.display_walks.walks.display_walks
