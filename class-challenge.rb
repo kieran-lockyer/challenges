4404c7c485f57bd5ec394f6ac783151dd4fd403d
kieran-lockyer <kieran.lockyer.87@gmail.com>
2018-08-30 15:44:15 +1000

    completed class challenge and beast mode. Doing Beast mode ++ in another file.

--- class-challenge.rb
+++ class-challenge.rb
@@ -1,5 +1,5 @@
 class Cat
-    def initialize(name, walks = 0)
+    def initialize(name)
         @name = name
         @walks = walks
     end
@@ -8,18 +8,66 @@ class Cat
         puts "#{@name} says meow."
         return self
     end
+end
 
-    def walks
-        @walks += 1
+# cat = Cat.new("Chairman Meow")
+
+# cat.speak.walks.walks.display_walks.walks.display_walks
+
+class Dog
+    require 'geocoder'
+    attr_accessor :breed, :age, :location, :breed
+
+    def initialize (dog_name, age, location = "Brisbane", breed = "Unkown", walks = 0)
+        @name = dog_name
+        @breed = breed
+        @walks = []
+        @age = age.to_i
+        @location = location
+        puts "Initialize running"
+    end
+
+    def speak
+        puts "#{@name} says Woof!"
+        self
+    end
+
+    # #setter
+    # def name=(name)
+    #     @name = name
+    # end
+
+    # #getter
+    def name
+        return @name
+        self
+    end
+
+    def walks(location, distance)
+        @walks.push([Time.now, distance, Geocoder.search(location).first.coordinates])
         return self
     end
 
     def display_walks
-        puts "#{@name} has been for #{@walks} walks today."
+        puts "#{@name} has been for #{@walks.count} walks today."
+        for walk in @walks
+            puts "At #{walk[0]}, #{@name} walked #{walk[1]}km's from #{Geocoder.search(walk[2]).first.address}"
+        end
         return self
     end
 end
 
-cat = Cat.new("Chairman Meow")
+dog1 = Dog.new("Fido", 2, "Brisbane", "Blue Heeler")
+# dog1.name = "Rover"
+lassie = Dog.new("Lassie", 4, "Melbourne", "Collie")
+# lassie.name = "Lassie"
 
-cat.speak.walks.walks.display_walks.walks.display_walks
+puts dog1.inspect
+puts lassie.inspect
+
+dog1.speak
+lassie.speak
+
+puts dog1.name
+
+dog1.walks("Brisbane", 20).walks("Brisbane", 10).display_walks
